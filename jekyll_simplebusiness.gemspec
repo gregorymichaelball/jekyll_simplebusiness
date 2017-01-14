07a80b071b265c1047a3979ba9a9c3f11677c85a
G-BOOK\Greg <greg.ball.cs@gmail.com>
2017-01-13 19:26:29 -0800

    carousel header complete

--- jekyll_simplebusiness.gemspec
+++ jekyll_simplebusiness.gemspec
@@ -3,11 +3,11 @@
 Gem::Specification.new do |spec|
   spec.name          = "jekyll_simplebusiness"
   spec.version       = "0.1.0"
-  spec.authors       = [""]
-  spec.email         = [""]
+  spec.authors       = ["Gregory Michael Ball"]
+  spec.email         = ["greg.ball.cs@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{simple business is a jekyll theme that uses bootstrap to create a clean but effective business page}
+  spec.homepage      = "https://github.com/gregorymichaelball/jekyll_simplebusiness"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files -z`.split("\x0").select { |f| f.match(%r{^(assets|_layouts|_includes|_sass|LICENSE|README)}i) }
---
+++ js/gmb.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Greg on 1/12/2017.
+ */
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+var animatedNavbar = (function() {
+
+    var docElem = document.documentElement,
+        header = document.querySelector( '.navbar-fixed-top' ),
+        didScroll = false,
+        changeHeaderOn = (window).innerHeight - 50;
+
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'navbar-shrink' );
+        }
+        else {
+            classie.remove( header, 'navbar-shrink' );
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
